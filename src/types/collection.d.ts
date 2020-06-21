b9490ea131a0400baaddd4ea4762819b4c915eea
gness1804 <graham.nessler@gmail.com>
2020-06-21 14:30:10 -0500

    Remove data property from CollectionI and add type check script

--- src/types/collection.d.ts
+++ src/types/collection.d.ts
@@ -1,5 +1,4 @@
 declare interface CollectionI {
-  data: number[];
   compare: (leftIndex: number, rightIndex: number) => boolean;
   swap: (leftIndex: number, rightIndex: number) => void;
   length: number;
