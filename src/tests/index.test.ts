b80772b4e2b2fb215514e80086a2b9e095f798a0
gness1804 <graham.nessler@gmail.com>
2020-07-11 15:36:03 -0500

    Refactor NumbersCollection.ts to use Sorter abstract class

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -3,11 +3,11 @@ import { Sorter } from './../Sorter';
 import { NumbersCollection } from './../NumbersCollection';
 
 describe('Sorter', () => {
-  // it('should properly sort an array of numbers', () => {
-  //   const collection = new NumbersCollection([9, 23, 3, -56, 2]);
-  //   collection.sort();
-  //   expect(collection.data).toEqual([-56, 2, 3, 9, 23]);
-  // });
+  it('should properly sort an array of numbers', () => {
+    const collection = new NumbersCollection([9, 23, 3, -56, 2]);
+    collection.sort();
+    expect(collection.data).toEqual([-56, 2, 3, 9, 23]);
+  });
 
   it('should properly sort an array of strings', () => {
     const collection = new CharactersCollection('eUaowwG');
