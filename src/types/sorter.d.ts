089a63605e737780fa35a31540c0aad64817cfa5
gness1804 <graham.nessler@gmail.com>
2020-07-11 15:53:21 -0500

    Fix interface sorter.d.ts

--- src/types/sorter.d.ts
+++ src/types/sorter.d.ts
@@ -1,6 +1,6 @@
 declare interface SorterI {
   sort: () => void;
-  // compare: (leftIndex: number, rightIndex: number) => boolean;
-  // swap: (leftIndex: number, rightIndex: number) => void;
-  // length: number;
+  compare: (leftIndex: number, rightIndex: number) => boolean;
+  swap: (leftIndex: number, rightIndex: number) => void;
+  length: number;
 }
